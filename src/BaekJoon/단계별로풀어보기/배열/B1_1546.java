e309f1628a6dcfd035d13152fde6799ed501e41a
Jkite0917 <deadvangers@naver.com>
2024-03-18 02:12:59 +0900

    2024_03_18_2 baekjoon

---
+++ src/BaekJoon/단계별로풀어보기/배열/B1_1546.java
@@ -0,0 +1,28 @@
+package BaekJoon.단계별로풀어보기.배열;
+
+import java.util.Scanner;
+public class B1_1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+        int[] scores = new int[count];
+
+        int max = 0;
+        for(int i=0; i<count; i++) {
+            scores[i] = sc.nextInt();
+            if(scores[i] > max) {
+                max = scores[i];
+            }
+        }
+        sc.close();
+
+        double result = 0.0;
+        for(int i=0; i<count; i++) {
+            result += (scores[i] / (double)max * 100);
+        }
+        System.out.println(result/count);
+
+    }
+}
+
